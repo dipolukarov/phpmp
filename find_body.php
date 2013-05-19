7e8e31f20aca505108b61b18358237dbf24bb98c
Denis I. Polukarov <dpolukarov@hostcomm.ru>
2013-05-19 17:42:49 +0400

    refactoring find

--- find_body.php
+++ find_body.php
@@ -1,40 +1,45 @@
 <?php
-include "info.php";
-include "config.php";
-include "utils.php";
-include "info2html.php";
-$dir = "";
-$find = "";
-$arg = "";
+/**
+ * @author neo <dipolukarov@gmail.com>
+ * @version $Id$
+ */
+
+include 'info.php';
+include 'config.php';
+include 'utils.php';
+include 'info2html.php';
+$dir = '';
+$find = '';
+$arg = '';
 $sort = $default_sort;
 extract($_GET);
-$sort_array = explode(",",$sort);
+$sort_array = explode(',',$sort);
 $fp = fsockopen($host,$port,$errno,$errstr,10);
 if(!$fp) {
-	echo "$errstr ($errno)<br>\n";
+	echo "$errstr ($errno)<br/>\n";
 }
 else {
 	while(!feof($fp)) {
 		$got =  fgets($fp,1024);
-		if(strncmp("OK",$got,strlen("OK"))==0) 
+		if(strncmp('OK',$got,strlen('OK'))==0) 
 			break;
-		print "$got<br>";
-		if(strncmp("ACK",$got,strlen("ACK"))==0) 
+		print "$got<br/>";
+		if(strncmp('ACK',$got,strlen('ACK'))==0) 
 			break;
 	}
 	if(isset($password)) {
 		fputs($fp,"password \"$password\"\n");
 		while(!feof($fp)) {
 			$got =  fgets($fp,1024);
-			if(strncmp("OK",$got,strlen("OK"))==0)
+			if(strncmp('OK',$got,strlen('OK'))==0)
 				break;
-			print "$got<br>";
-			if(strncmp("ACK",$got,strlen("ACK"))==0) 
+			print "$got<br/>";
+			if(strncmp('ACK',$got,strlen('ACK'))==0) 
 				break;
 		}
 	}
 	$dir_url = sanitizeForURL($dir);
-	displayDirectory($dir,$sort,"Back to Directory",0,0);
+	displayDirectory($dir,$sort,'Back to Directory',0,0);
 	$lsinfo = getLsInfo($fp,"find $find \"$arg\"\n");
 	list($mprint,$mindex,$add_all) = lsinfo2musicTable($lsinfo,$sort,$dir_url);
 	$arg_url = sanitizeForURL($arg);
@@ -43,4 +48,3 @@ else {
 	displayStats($dir,$sort);
 	displayUpdate($dir,$sort);
 }
-?>
